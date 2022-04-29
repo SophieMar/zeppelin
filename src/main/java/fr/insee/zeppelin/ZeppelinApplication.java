7909e1a26bea5291102d303b81f843de46dc2358
Sophie Marziali <sophie.marziali@insee.fr>
2022-04-29 15:00:32 +0200

    initialize

---
+++ src/main/java/fr/insee/zeppelin/ZeppelinApplication.java
@@ -0,0 +1,13 @@
+package fr.insee.zeppelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeppelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeppelinApplication.class, args);
+	}
+
+}
